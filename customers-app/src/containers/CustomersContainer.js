94e016c74f144ad52dcd68e4ad13429ef56df89d
murmu <mauro.mosconi@redb.ee>
2019-11-07 17:36:54 -0300

    cambios

--- customers-app/src/containers/CustomersContainer.js
+++ customers-app/src/containers/CustomersContainer.js
@@ -13,7 +13,10 @@ import { getCustomers } from '../selectors/customers';
 class CustomersContainer extends Component {
 
     componentDidMount() {
-        this.props.fetchCustomers();
+        if(this.props.customers.length === 0){
+            this.props.fetchCustomers();
+        }
+        
     }
     
     handleAddNew = () =>{
--- customers-app/src/reducers/customers.js
+++ customers-app/src/reducers/customers.js
@@ -1,10 +1,21 @@
 import {handleActions} from 'redux-actions';
-import {FETCH_CUSTOMERSRS } from '../constants';
+import {FETCH_CUSTOMERSRS, CREATE_CUSTOMER, UPDATE_CUSTOMER } from '../constants';
 
 
 
 export const customers = handleActions({
     
     [FETCH_CUSTOMERSRS]: (state, action) =>{ 
-        return [...action.payload]}
+        return [...action.payload]},
+    [CREATE_CUSTOMER]:(state,action) => {
+        return [...state,action.payload]},
+    [UPDATE_CUSTOMER]:(state,action) =>{
+        const payload = action.payload;
+        return state.reduce((acc,customer) =>{
+            if(customer.id === payload.id){
+                return [...acc,payload]
+            }
+            else return [...acc,customer]
+        },[]) 
+    }   
 },[]);
